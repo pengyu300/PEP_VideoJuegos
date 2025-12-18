c32c0e1494056dca939a3ea24dfa9780157952d5
Qixian Guan <151907247+nanbeiyyds@users.noreply.github.com>
2025-12-18 10:30:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+"""main.py
+
+Punto de entrada del programa.
+
+Este archivo solo llama al menú principal.
+
+Se ejecuta desde la línea de comandos así:
+
+    python main.py
+
+(asegurándote de que estás en la carpeta donde está este archivo).
+"""
+
+from gestor.menu import menu_principal
+
+
+if __name__ == "__main__":
+    menu_principal()
